d1ae4c3b57879748b9922456ebea0327c34a230e
Warren Dodsworth <warren.dodsworth@gmail.com>
2024-12-05 20:41:21 +1100

    update to inference api

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from dotenv import find_dotenv, load_dotenv
 import os
 import streamlit as st
-import torch
+from huggingface_hub import InferenceClient
 
 # Set the page configuration as the first Streamlit command
 st.set_page_config(page_title="Smush Date", page_icon="♥️")
@@ -13,30 +12,6 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 # Load environment variables
 load_dotenv(find_dotenv())
 HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-device = 0 if torch.backends.mps.is_available() else -1
-
-
-# Load the Hugging Face model for conversational AI (e.g., GPT-2, GPT-3 fine-tuned)
-@st.cache_resource
-def load_model():
-    tokenizer = AutoTokenizer.from_pretrained(
-        "google/gemma-2-2b-it",
-        padding_side="left",
-    )
-    model = AutoModelForCausalLM.from_pretrained(
-        "google/gemma-2-2b-it",
-    )
-    nlp_pipeline = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        token=HUGGINGFACEHUB_API_TOKEN,
-        device=device,
-    )
-    return nlp_pipeline
-
-
-model_pipeline = load_model()
 
 # Add context for the chatbot to be a good listener
 system_message = """You are a relationship expert who listens carefully before providing advice. 
@@ -49,6 +24,42 @@ if "history" not in st.session_state:
     st.session_state.history = []
 if "user_input" not in st.session_state:
     st.session_state.user_input = ""
+if "is_processing" not in st.session_state:
+    st.session_state.is_processing = False
+
+
+# Call the API
+def model_api(user_input: str, system_message: str):
+    # model = "google/gemma-2-2b-it"
+    client = InferenceClient(api_key=HUGGINGFACEHUB_API_TOKEN)
+    messages = [
+        {"role": "user", "content": user_input},
+        {"role": "system", "content": system_message},
+    ]
+    completion = client.chat.completions.create(
+        model="Qwen/Qwen2.5-72B-Instruct", messages=messages, max_tokens=500
+    )
+    # print(completion.choices[0].message)
+    return completion.choices[0].message.content
+
+
+# Chat function
+def generate_response(user_input):
+    st.session_state.is_processing = True  # Set processing state to True
+
+    # Simulate loading by showing spinner in place of the text box
+    with input_container:
+        st.markdown("💬 Thinking...")
+
+    # Call the API
+    conversation_history = "".join(
+        f"User: {chat['user']}\nBot: {chat['bot']}\n"
+        for chat in st.session_state.history
+    )
+    prompt = f"Conversation History: {conversation_history}. User:{user_input}"
+    response = model_api(prompt, system_message=system_message)
+    st.session_state.history.append({"user": user_input, "bot": response})
+    st.session_state.is_processing = False  # Reset processing state
 
 
 # Function to clear user input
@@ -56,27 +67,12 @@ def clear_input():
     st.session_state.user_input = ""
 
 
-# Chat function
-def generate_response(user_input):
-    conversation_history = "".join(
-        f"User: {chat['user']}\nBot: {chat['bot']}\n"
-        for chat in st.session_state.history
-    )
-    prompt = f"{system_message}\n{conversation_history}User: {user_input}\nBot:"
-    response = model_pipeline(prompt, max_new_tokens=150, num_return_sequences=1)[0][
-        "generated_text"
-    ]
-    response = response.replace(prompt, "").strip()  # Clean up the response
-    st.session_state.history.append({"user": user_input, "bot": response})
-
-
 # Sidebar for chat settings
 with st.sidebar:
     st.sidebar.header("Chat Settings")
     if st.sidebar.button("Reset Chat"):
-        # Clear chat history and user input when the reset button is clicked
-        st.session_state.history = []  # Clear the history
-        st.session_state.user_input = ""  # Clear user input
+        st.session_state.history = []
+        st.session_state.user_input = ""
 
 # Display chat history
 st.title("Smush Date - Relationship Chatbot")
@@ -85,9 +81,17 @@ for chat in st.session_state.history:
     st.markdown(f"**Bot:** {chat['bot']}")
     st.markdown("---")
 
-# User input section
-st.text_input(
-    "Type your message:",
-    key="user_input",
-    on_change=lambda: (generate_response(st.session_state.user_input), clear_input()),
-)
+# Container for user input and spinner
+input_container = st.empty()
+
+if st.session_state.is_processing is False:
+    # Display the text box
+    with input_container:
+        st.text_input(
+            "Talk to me",
+            key="user_input",
+            on_change=lambda: (
+                generate_response(st.session_state.user_input),
+                clear_input(),
+            ),
+        )
